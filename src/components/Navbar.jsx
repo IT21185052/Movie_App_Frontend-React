14b99b529d3efb4b20d1ea46b567c507a154d575
S.Y.T.D.Bandara <it21185052@my.sliit.lk>
2025-04-24 16:43:59 +0530

    change the navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,17 +1,21 @@
-
 import { Link } from "react-router-dom";
-import "../css/Navbar.css"
+import "../css/Navbar.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faFilm } from "@fortawesome/free-solid-svg-icons";
 
 function NavBar() {
-    return <nav className="navbar">
-        <div className="navbar-brand">
-            <Link to="/">Movie App</Link>
-        </div>
-        <div className="navbar-links">
-            <Link to="/" className="nav-link">Home</Link>
-            <Link to="/favorites" className="nav-link">Favorites</Link>
-        </div>
-    </nav>
+    return (
+        <nav className="navbar">
+            <div className="navbar-brand">
+                <FontAwesomeIcon icon={faFilm} className="logo-icon" />
+                <Link to="/">Movie App</Link>
+            </div>
+            <div className="navbar-links">
+                <Link to="/" className="nav-link">Home</Link>
+                <Link to="/favorites" className="nav-link">Favorites</Link>
+            </div>
+        </nav>
+    );
 }
 
-export default NavBar
+export default NavBar;
